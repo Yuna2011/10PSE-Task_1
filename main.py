ee679275b423babaa3e9817d383e6067f0360d2b
Yuna2011 <yuna.shin1@education.nsw.gov.au>
2026-03-19 01:32:37 +1100

    feat : main
    Just changed the words to the correct words I needed.

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ from functions import*
 
 def main():
     while True:
-        print("\nPokédex Menu:")
-        print("1. Search Pokémon")
-        print("2. Add Pokémon to Pokédex")
-        print("3. View Pokédex")
-        print("4. Remove Pokémon")
+        print("\F1 Menu:")
+        print("1. Search Driver")
+        print("2. Add Driver to List")
+        print("3. View Driver List")
+        print("4. Remove Driver")
         print("5. Exit")
         choice = input("Choose an option: ")
 
         if choice == "1":
-            name = input("Enter Pokémon name or ID: ")
+            name = input("Enter driver name: ")
             print(f"Search for {name}'s details") #Replace with function later
         elif choice == "2":
-            name = input("Enter Pokémon name to add: ")
-            print(f"Add {name} to Pokedex") #Replace with function later
+            name = input("Enter driver name to add: ")
+            print(f"Add {name} to driver list") #Replace with function later
         elif choice == "3":
-            print('View Pokedex') #Replace with function later
+            print('View driver list') #Replace with function later
         elif choice == "4":
-            name = input("Enter Pokémon name to remove: ")
-            print(f'Remove {name} from Pokedex') #Replace with function later
+            name = input("Enter driver name to remove: ")
+            print(f'Remove {name} from driver list') #Replace with function later
         elif choice == "5":
-            print("Exiting Pokédex.")
+            print("Exiting driver list.")
             break
         else:
             print("Invalid choice. Please try again.")
